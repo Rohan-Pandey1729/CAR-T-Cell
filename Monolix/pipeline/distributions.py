3c5f95f352f87040ba9d87a0b8b88b2308728317
Ray Chen <raychen256@gmail.com>
2025-07-12 01:34:17 -0700

    pipeline updates

--- Monolix/pipeline/distributions.py
+++ Monolix/pipeline/distributions.py
@@ -1,27 +1,57 @@
 import random
-from math import log, exp
+from math import exp, log
+from typing import Callable
 
 
-def _clamp(val: float, low: float | None, high: float | None):
-    if low and high and (low > high):
-        raise ValueError("incompatible bounds")
-    if high:
-        val = min(val, high)
-    if low:
-        val = max(val, low)
-    return val
-
-
-def normal(
-    mu: float, sigma: float, low: float | None = None, high: float | None = None
+def truncate(
+    distribution_sampler: Callable[[], float],
+    low: float | None = None,
+    high: float | None = None,
 ):
-    return _clamp(random.gauss(mu, sigma), low, high)
+    """
+    Returns a new function which repeatedly samples values using
+    `distribution_sampler` until the given value is at least `low`
+    (if given) and at most `high` (if given)
+    """
+    if low and high and low > high:
+        raise ValueError("low must be <= high")
+
+    if not low:
+        low = -float("inf")
+
+    if not high:
+        high = float("inf")
+
+    def truncated_sampler():
+        val = distribution_sampler()
+        while not (low <= val <= high):
+            val = distribution_sampler()
+        return val
+
+    return truncated_sampler
 
 
-def lognormal(
-    mu: float, sigma: float, low: float | None = None, high: float | None = None
-):
+def normal(mu: float, sigma: float):
+    """
+    Returns a function that samples from a normal distribution
+    with mean `mu` and standard deviation `sigma`
+    """
+    return lambda: random.gauss(mu, sigma)
+
+
+def lognormal(mu: float, sigma: float):
+    """
+    Returns a function that samples from a lognormal distribution
+    with mean `mu` and where the log of the associated random variable
+    has standard deviation `sigma`
+    """
     if mu <= 0:
         raise ValueError("mean can't be <= 0 if using lognormal distribution")
     noise = random.gauss(sigma=sigma)
-    return _clamp(exp(log(mu) + noise), low, high)
+    return lambda: exp(log(mu) + noise)
+
+def constant(val: float):
+    """
+    Returns a function that always returns `val`
+    """
+    return lambda: val
--- Monolix/pipeline/noise_funcs.py
+++ Monolix/pipeline/noise_funcs.py
@@ -7,33 +7,40 @@ from distributions import normal, lognormal
 def proportional_normal_noise(noise_level: float, lower_bound: float | None = None):
     """
     the noisy observation will be normally distributed with ground truth as mean
-    and |(ground truth) * noise_level| as standard deviation
+    and |(ground truth) * noise_level| as standard deviation and will be
+    set to `lower_bound` if lower
     """
 
-    if noise_level < 0 or lower_bound:
+    if noise_level < 0:
         raise ValueError("expected nonnegative noise value")
+    
+    if lower_bound is None:
+        lower_bound = -float("inf")
 
-    def noise_func(obs: float, seed: int):
+    def noise_func(obs: float, seed: int) -> float:
         random.seed(seed)
-        noisy_obs = normal(mu=obs, sigma=abs(noise_level * obs))
+        noisy_obs = normal(mu=obs, sigma=abs(noise_level * obs))()
         return max(noisy_obs, lower_bound)
 
     return noise_func
 
 
-def constant_lognormal_noise(sigma: float, lower_bound: float):
+def constant_lognormal_noise(sigma: float, lower_bound: float | None = None):
     """
     sigma is standard deviation of the log of the noisy observation
     (which is a normally distributed random variable)
     """
 
-    def noise_func(obs: float, seed: int) -> int:
+    if lower_bound is None:
+        lower_bound = -float("inf")
+
+    def noise_func(obs: float, seed: int) -> float:
         if obs <= 0:
             raise ValueError(
                 "observations can't be <= 0 if using lognormal distribution"
             )
         random.seed(seed)
-        noisy_obs = lognormal(obs, sigma)
+        noisy_obs = lognormal(obs, sigma)()
         return max(noisy_obs, lower_bound)
 
     return noise_func
